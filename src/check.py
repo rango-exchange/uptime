cafd4c87bd8124ecdd2c55ecdbb70f53a8e65f06
RanGojo <rangojo@rango.exchange>
2023-08-15 12:37:45 +0300

    fix: update quotes samples and filter paused ones

--- src/check.py
+++ src/check.py
@@ -12,17 +12,20 @@ if not UPTIME_ROBOT_API_KEY:
     raise Exception("UPTIME_ROBOT_API_KEY not found in environment variables")
 
 routes = get_sample_routes()
-dex_routes = routes['dexes']
-bridges_routes = routes['bridges']
+dex_routes = [route for route in routes['dexes'] if not route.get('paused')]
+bridges_routes = [route for route in routes['bridges'] if not route.get('paused')]
 routes_list = dex_routes + bridges_routes
 
 for route in routes_list:
-    get_quote_or_throw_exception(
-        RANGO_API_KEY,
-        route['from'], 
-        route['to'], 
-        route['amount'], 
-        route['swappers'], 
-        3
-    )
+    try:
+        get_quote_or_throw_exception(
+            RANGO_API_KEY,
+            route['from'], 
+            route['to'], 
+            route['amount'], 
+            route['swappers'], 
+            3
+        )
+    except:
+        pass
     # time.sleep(0.5)
